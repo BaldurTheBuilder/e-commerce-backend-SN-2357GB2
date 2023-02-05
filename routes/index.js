4a898835d08f67548dc810ad17066b9fa263215f
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-02-04 21:32:04 -0800

    bugfixing complete

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+
+// WHEN I open API GET routes in Insomnia Core for categories, products, or tags
+// THEN the data for each of these routes is displayed in a formatted JSON
+
+// WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
+// THEN I am able to successfully create, update, and delete data in my database
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
